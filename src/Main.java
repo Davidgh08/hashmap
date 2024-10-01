cce01d00114de24e52706958de591d4d42bb37ab
Davidgh08 <jdgherrera2005@gmail.com>
2024-10-01 16:11:32 -0500

    hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        //Juan David Garzon Herrera
-        //890802
 
-        System.out.println("hola");
+        HashMap<Integer, String> hash = new HashMap<>();
 
+        hash.put(19,"Juan");
+        hash.put(30,"Maria");
+        hash.put(27,"Pedro");
+
+        String nombre1 = hash.get(19);
+        String nombre2 = hash.get(30);
+        String nombre3 = hash.get(27);
+
+
+        personas p1 = new personas(nombre1,29);
+        personas p2 = new personas(nombre2, 20);
+        personas p3 = new personas(nombre3, 50);
+
+        System.out.println(hash);
+
+            //Juan David Garzon Herrera
+            //890802
     }
 }
